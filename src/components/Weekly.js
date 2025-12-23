85c9220fd0087119e2e47d18f4d1f2859b3b4211
Xavier M. <xavier.mcin@gmail.com>
2025-12-22 22:18:15 -0500

    rough draft styles for wekkly section

--- src/components/Weekly.js
+++ src/components/Weekly.js
@@ -1,12 +1,42 @@
 import React, { useEffect, useState } from 'react'
+import hot from '../icons/weather/hot.png';
 
 export default function Weekly() {
 
   const [days, setDays] = useState([]);
 
   useEffect(() => {
-    setDays(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+    setDays(['Next 7 Days', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
   }, []);
+
+  function wd_elements(num) {
+    if (num === 0) {
+      return  (
+        <>
+          <p>{days[num]}</p>
+          <div><p>Forecast</p></div>
+        </>
+      )
+    } else if (num === 1) {
+      return  (
+        <>
+          <div><p>{days[num]}</p><div><p>11:00 PM</p></div></div>
+          <div>
+            <p>10°</p>
+            <div><img src={hot} alt="" /></div>
+          </div>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <div><p>{days[num]}</p></div>
+          <div><img src={hot} alt="" /></div>
+          <div><p>10°</p></div>
+        </>
+      )
+    }
+  }
 
   return (
     <section className='weekly'>
@@ -14,7 +44,11 @@ export default function Weekly() {
         { days.map((curr, index) => {
           
           return (
-            <li className={ index === 0 ? 'weekday today' : 'weekday'}></li>
+            <li className={ index === 0 ? 'week_title' : index === 1 ? 'today' : 'weekday'}>
+              {
+                wd_elements(index)
+              }
+            </li>
           )
           
         })}
